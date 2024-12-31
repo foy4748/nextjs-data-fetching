fcde860f779346d3a072c8538147c7faa920bcc3
foy4748 <faisaljfcl@gmail.com>
2024-12-31 20:46:19 +0600

    Data Fetching using SSR

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,27 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+
+export default function NavBar() {
+  const pathname = usePathname();
+  console.log(pathname, pathname.includes("dashboard"));
+  if (!pathname.includes("dashboard")) {
+    return (
+      <div>
+        <nav className="flex justify-center">
+          <ul className="flex justify-between w-1/2">
+            <Link href="/">
+              <li>Home</li>
+            </Link>
+            <Link href="/posts">
+              <li>Posts</li>
+            </Link>
+          </ul>
+        </nav>
+      </div>
+    );
+  } else {
+    return <></>;
+  }
+}
