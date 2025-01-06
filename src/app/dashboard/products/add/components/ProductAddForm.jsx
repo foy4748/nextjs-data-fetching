ce2052c0dde67e348a9f8df4e6c47841b36b3fcb
foy4748 <faisaljfcl@gmail.com>
2025-01-07 01:44:32 +0600

    Debug deployment and Server Actions

--- src/app/dashboard/products/add/components/ProductAddForm.jsx
+++ src/app/dashboard/products/add/components/ProductAddForm.jsx
@@ -1,22 +1,25 @@
 "use client";
 
+import { postSingleData } from "@/app/actions/products/postSingleProduct";
 import { useRouter } from "next/navigation";
 
 export default function ProductAddForm() {
+  const { NEXT_PUBLIC_SERVER_ADDRESS } = process.env;
   const router = useRouter();
   const handleSubmit = async (e) => {
     e.preventDefault();
     const form = e.target;
     const productName = form.productName.value;
     const payload = { productName };
-    const res = await fetch("http://localhost:3000/api/items", {
-      method: "POST",
-      body: JSON.stringify(payload),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    const result = await res.json();
+    // const res = await fetch(`${NEXT_PUBLIC_SERVER_ADDRESS}/api/items`, {
+    //   method: "POST",
+    //   body: JSON.stringify(payload),
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    // });
+    // const result = await res.json();
+    const result = await postSingleData(payload);
     console.log(result);
     form.reset();
     //alert("Product Added");
--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -1,11 +1,16 @@
+import dbConnect from "@/lib/dbConnect";
 import { redirect } from "next/navigation";
 import React from "react";
+import { getProducts } from "../actions/products/getProducts";
 
-export const dynamic = "force-dynamic";
+// export const dynamic = "force-dynamic";
 
 export default async function ProductsPage() {
-  const res = await fetch("http://localhost:3000/api/items");
-  const data = await res.json();
+  // const { NEXT_PUBLIC_SERVER_ADDRESS } = process.env;
+  // const res = await fetch(`${NEXT_PUBLIC_SERVER_ADDRESS}/api/items`);
+  // const data = await res.json();
+
+  const data = await getProducts();
 
   return (
     <ul className="text-center mt-8">
